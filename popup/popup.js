99f5de63c8bc0fe23d41e3cc1214b4f7f16bf1b7
Leo Dauga <ldauga@z3r2p2.42lyon.fr>
2024-06-14 18:08:20 +0200

    commit

---
+++ popup/popup.js
@@ -0,0 +1,39 @@
+const buttons = {
+	login: document.getElementById('loginButton'),
+	logout: document.getElementById('logoutButton')
+}
+
+function onLogout() {
+	chrome.storage.local.set({ 'access_token': '' })
+	window.close()
+}
+
+
+
+chrome.storage.local.get('access_token', ({ access_token }) => {
+
+	console.log(access_token)
+
+	if (!access_token) {
+		console.log(chrome.storage)
+		buttons.login.addEventListener('click', async () => {
+			const popup = window.open('', '_blank', 'width=600,height=600');
+
+			window.addEventListener('message', function (event) {
+				const accessToken = event.data.access_token;
+				if (accessToken) {
+					chrome.storage.local.set({ 'access_token': accessToken });
+				}
+			});
+			fetch("http://144.24.205.159:8000/users/login").then(res => res.json()).then(url => popup.location.href = url)
+		});
+	}
+	else {
+		fetch("http://144.24.205.159:8000/users/me", {headers: {Authorization: `Bearer ${access_token}`}}).then(res => res.json()).then(user => {
+			buttons.login.style = "display: none;"
+			buttons.logout.style = "display: block;"
+			buttons.logout.addEventListener('click', onLogout)
+			document.getElementById('title').textContent += user.login + ' !'
+		}).catch(err => onLogout())
+	}
+})
